39ee1a1c6c7aaefe3c5434578c84b5ebe8ccf685
Evan Widloski <evan@evanw.org>
2015-10-03 11:33:26 -0400

    added compile file

---
+++ compile.sh
@@ -0,0 +1,1 @@
+java -cp compilers/ecj.jar org.eclipse.jdt.internal.compiler.batch.Main -deprecation -g -source 1.6 -encoding UTF-8 -classpath ':libs/robocode.jar:./robots' robots/ieee/MoistPits.java
